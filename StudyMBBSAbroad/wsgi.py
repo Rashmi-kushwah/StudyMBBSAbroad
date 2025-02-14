22fe87bf11ef2ee126da5f0bd6c53132894770ce
Rashmi Kushwah <rashmiinfo6@gmail.com>
2025-02-14 13:02:10 +0530

    Initial commit

--- StudyMBBSAbroad/wsgi.py
+++ StudyMBBSAbroad/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StudyMBBSAbroad.settings')
 
 application = get_wsgi_application()
+# Vercel ke liye handler define karein
+app = application
